95764550c1ad7d8195193d6e9f803a8e1fa5e422
Jesse Heckman <jlheckman@gmail.com>
2022-09-23 17:16:19 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# INF601 - Advanced Programming in Python
+# Jesse Heckman
+# Mini Project 2
+
+#This project is designed to build out some data frames using PANDAS
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
