3603295cace5ade485d8e8865f7722693f246a70
Jesse Heckman <jlheckman@gmail.com>
2022-09-25 11:11:24 -0500

    Added made up data with np random

--- main.py
+++ main.py
@@ -9,4 +9,24 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+madeupdata = pd.Series(np.random.randn(1000), index=pd.date_range("1/1/2000", periods=1000))
+
+madeupdata = madeupdata.cumsum()
+
+madeupdata.plot()
+
+df = pd.DataFrame(
+     np.random.randn(1000, 4), index=madeupdata.index, columns=["Plant 1", "Plant 2", "Plant 3", "Plant 4"]
+        )
+
+df = df.cumsum()
+
+plt.figure()
+
+df.plot()
+
+plt.legend(loc='best')
+
+plt.show()
+
 
